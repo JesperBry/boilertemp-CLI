c79526bab80abc400ea759e593bc8b36e6a4ce08
Jesper Brynildsen <jesper_bry@hotmail.com>
2020-10-03 22:14:59 +0200

    Change mern + redux temp to use Hooks

--- src/templates/mern + redux/client/src/App.js
+++ src/templates/mern + redux/client/src/App.js
@@ -6,30 +6,28 @@ import store from "./store";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      // Provider: wraps the React application and makes the Redux state available to all container components in the application’s hierarchy
-      <Provider store={store}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      </Provider>
-    );
-  }
-}
+const App = () => {
+  return (
+    // Provider: wraps the React application and makes the Redux state available to all container components in the application’s hierarchy
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Provider>
+  );
+};
 
 export default App;
